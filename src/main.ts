d4600d5ef0510f70e5e2c093efbd87f3dc27a389
James Arnold <james@pairgo.co.uk>
2022-02-18 10:14:36 +0000

    cleaned up

--- src/main.ts
+++ src/main.ts
@@ -1,81 +1,88 @@
-import './style.css'
-import calls from './bingo_calls.json'
+import "./style.css";
+import calls from "./bingo_calls.json";
 
-let previousNumbers: number[] = []
+let previousNumbers: number[] = [];
+
+function retrieveElement(selector: string): Element {
+  let el = document.querySelector(selector);
+  if (!el) {
+    throw new Error(`Unable to retrieve element with selector: ${selector}`);
+  }
+
+  return el;
+}
 
 function restartGame(): void {
-  previousNumbers = []
+  let currentNumberEl = retrieveElement(".number-section_current-number");
+  currentNumberEl.innerHTML = "BINGOOOO";
+
+  let previousSectionDiv = retrieveElement(".previous-section");
+
+  previousSectionDiv.innerHTML = "";
+  previousNumbers = [];
+
+  let currentNumberDiv = retrieveElement(".number-section");
+  let callEl = currentNumberDiv.lastChild;
+  if (callEl) {
+    currentNumberDiv.removeChild(callEl);
+  }
 }
 
 function restartGameAction(): void {
-  let currentNumberEl = document.querySelector('.number-section_current-number')
-  if (!currentNumberEl) {
-    throw new Error("Unable to get number, cannot find current number element");
-  }
-  restartGame()
-  currentNumberEl.innerHTML = "BINGOOOO"
-
-  let previousSectionDiv = document.querySelector('.previous-section')
-  if (!previousSectionDiv) {
-    throw new Error("Unable to retrieve revious section div");
-  }
-
-  previousSectionDiv.innerHTML = ''
+  restartGame();
 }
 
 function getNumberAction() {
-  let currentNumberDiv = document.querySelector('.number-section')
-  if (!currentNumberDiv) {
-    throw new Error("Unable to get number, cannot find current number element");
-  }
-  let number = generateNumber()
-  let currentNumberEl = document.createElement('h1')
-  currentNumberEl.classList.add('number-section_current-number')
-  currentNumberEl.innerHTML = String(number)
+  let currentNumberDiv = retrieveElement(".number-section");
 
+  let number = generateNumber();
 
-  currentNumberDiv.innerHTML = ''
-  currentNumberDiv.appendChild(currentNumberEl)
+  let currentNumberEl = document.createElement("h1");
+  currentNumberEl.classList.add("number-section_current-number");
+  currentNumberEl.innerHTML = String(number);
 
-  let sayingEl = document.createElement('h3')
-  sayingEl.innerText = getSaying(number)
-  
-  currentNumberDiv.appendChild(sayingEl)
-  
+  currentNumberDiv.innerHTML = "";
+  currentNumberDiv.appendChild(currentNumberEl);
+
+  let sayingEl = document.createElement("h3");
+  sayingEl.innerText = getSaying(number);
+
+  currentNumberDiv.appendChild(sayingEl);
 }
 
 function getSaying(num: number): string {
   if (num < 1 || num > 90) {
-    throw new Error("Number must be between 1-90")
+    throw new Error("Number must be between 1-90");
   }
   //@ts-ignore
-  let call = calls[String(num)]
+  let call = calls[String(num)];
 
-  return call
+  return call;
 }
 
 function generateNumber(): number {
   let newNumber = false;
   let selectedNumber: number = 0;
   while (!newNumber) {
-    let numberCandidate = Math.ceil(Math.random() * 90)
+    let numberCandidate = Math.ceil(Math.random() * 90);
     if (!previousNumbers.includes(numberCandidate)) {
-      newNumber = true
-      selectedNumber = numberCandidate
+      newNumber = true;
+      selectedNumber = numberCandidate;
     }
   }
 
-  previousNumbers.push(selectedNumber)
-  let previousSection = document.querySelector('.previous-section')
-  if (!previousSection) {
-    throw new Error("Unable to retrieve previous section div")
-  }
+  previousNumbers.push(selectedNumber);
+  let previousSection = retrieveElement(".previous-section");
 
-  let prevNumberEl = document.createElement('p')
-  prevNumberEl.innerText = String(selectedNumber)
-  previousSection.appendChild(prevNumberEl)
+  let prevNumberEl = document.createElement("p");
+  prevNumberEl.innerText = String(selectedNumber);
+  previousSection.appendChild(prevNumberEl);
   return selectedNumber;
 }
 
-document.getElementById('control-section_get-number')?.addEventListener('click', () => getNumberAction())
-document.getElementById('control-section_restart')?.addEventListener('click', () => restartGameAction())
+document
+  .getElementById("control-section_get-number")
+  ?.addEventListener("click", () => getNumberAction());
+document
+  .getElementById("control-section_restart")
+  ?.addEventListener("click", () => restartGameAction());
